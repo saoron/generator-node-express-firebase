814fea4e35f92d7c3acbb3b055292ece9348ec26
Yossi <saoron@gmail.com>
2016-09-12 14:53:08 +0300

    Added example to controller

--- endpoint/templates/name.controller.js
+++ endpoint/templates/name.controller.js
@@ -16,3 +16,43 @@ exports.index = function(req, res) {
 };
 
 
+
+/* 
+* Example 
+*/
+
+// exports.index = function(req, res) {
+
+// 	var token = (req.param('token')) 
+
+
+
+// 	firebase.auth().verifyIdToken(token).then(function(decodedToken) {
+// 	    var uid = decodedToken.uid;
+// 	    var db = firebase.database();
+// 	    var ref = db.ref("userData").child(uid);
+
+
+// 	    ref.once("value", function(snapshot) {
+// 	    	//output the data
+// 	    	if (snapshot.val())
+// 	    		res.json({status: 1, userObject: snapshot.val()});
+// 	    	else
+// 	    		res.json({status: 0});
+// 	    });
+
+// 	}).catch(function(error) {
+// 	    // Handle error
+// 	 	var ret = {'status': 0};
+
+// 		if (config.env == 'development'){
+// 			ret['info'] = error;
+// 		}
+// 		res.json(ret);
+// 	});
+
+
+
+
+
+// };
