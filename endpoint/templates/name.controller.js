f34348fa9b3711674eaafa5d954f09983287579a
Yossi <saoron@gmail.com>
2016-09-12 14:47:36 +0300

    clean controller template

--- endpoint/templates/name.controller.js
+++ endpoint/templates/name.controller.js
@@ -7,15 +7,12 @@ var validator = require('validator');
 
 var config = require('../../config/environment');
 
-var <%= classedName %> = require('./<%= name %>.model');<% } %>
 
 // Get list of <%= name %>s
-exports.index = function(req, res) {<% if (!filters.mongoose) { %>
-  res.json([]);<% } %><% if (filters.mongoose) { %>
-  <%= classedName %>.find(function (err, <%= name %>s) {
-    if(err) { return handleError(res, err); }
-    return res.status(200).json(<%= name %>s);
-  });<% } %>
-};<% if (filters.mongoose) { %>
+exports.index = function(req, res) {
+
+  res.json([]);
+
+};
 
 
